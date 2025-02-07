d20ee7f5b34a63663582ef725e5bad84ab769849
Neven Mitrovic <153770602+nevenMitrovic@users.noreply.github.com>
2025-02-07 19:38:47 +0100

    chore: capacitor/assets

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,25 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'fitness-scheduling',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: false,
+      launchFadeOutDuration: 3000,
+      backgroundColor: '#a538ff',
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: true,
+      androidSpinnerStyle: 'large',
+      iosSpinnerStyle: 'small',
+      spinnerColor: '#7060ff',
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: 'launch_screen',
+      useDialog: true,
+    },
+  },
 };
 
 export default config;
